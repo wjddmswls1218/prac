0301d334af03a0983e6d34079bb25af57e378e84
wjddmswls1218 <ouhouh2222@gmail.com>
2021-11-06 14:26:46 +0900

    a

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const checkLogin = require("../middlewares/checkLogin");
+const db = require("../db");
 
 const router = express.Router();
 
@@ -11,7 +12,43 @@ router.get("/", checkLogin, (req, res, next) => {
 
 router.get("/signup", checkLogin, (req, res, next) => {
   const loggedIn = req.session.isLoggedIn;
+
   res.render("screens/signup", { loggedIn });
+});
+
+router.post("/signup", (req, res, next) => {
+  const insertQuery = `
+  INSERT INTO users (
+    email,
+    password,
+    mobile,
+    name,
+    createdAt,
+    updatedAt
+  ) VALUES (
+    "${req.body.input_email}",
+    "${req.body.input_password}",
+    "${req.body.input_mobile}",
+    "${req.body.input_name}",
+    now(),
+    now()
+  )
+  `;
+
+  try {
+    db.query(insertQuery, (err, rows) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (!err) {
+        return res.redirect("/");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("회원가입에 실패하셨습니다.");
+  }
 });
 
 router.get("/signin", checkLogin, (req, res, next) => {
